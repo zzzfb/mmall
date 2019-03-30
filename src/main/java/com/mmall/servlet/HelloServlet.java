7c710bcaf6c63ab1c3ae07786d269e501eefab41
zfb <zfb>
2019-03-30 23:03:42 +0800

    servlet first commit

---
+++ src/main/java/com/mmall/servlet/HelloServlet.java
@@ -0,0 +1,38 @@
+package com.mmall.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class HelloServlet implements Servlet {
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("helloServlet");
+    }
+
+
+
+
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
