e16fcaa8a387b544b4ef024eef80415c56e3e23a
zfb <zfb>
2019-03-31 18:41:18 +0800

    servlet应用：第一版： 在servlet中写html作为结果返回

---
+++ src/main/java/com/mmall/servlet/BeerServlet.java
@@ -0,0 +1,18 @@
+package com.mmall.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class BeerServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out=resp.getWriter();
+        String str=req.getParameter("color");
+        out.println("<br>your choice is"+str);
+    }
+}
